da49657bf96cee6247f1f3ff5496578d81bb49df
Nicolas Bigaouette <nbigaouette@gmail.com>
2019-12-03 17:39:42 -0500

    Add day03

--- day03/src/initial.rs
+++ day03/src/initial.rs
@@ -1,8 +1,146 @@
+use std::ops::Add;
+
 use crate::*;
 
 #[derive(Debug)]
 pub struct Day03Initial<'a> {
     input: &'a str,
+    displacements: Vec<Vec<Day03Entry>>,
+}
+
+#[derive(Debug, Default, Clone)]
+struct Location {
+    x: isize,
+    y: isize,
+}
+
+#[allow(clippy::suspicious_arithmetic_impl)]
+impl Add<&Day03Entry> for &Location {
+    type Output = Location;
+
+    fn add(self, other: &Day03Entry) -> Location {
+        match other {
+            Day03Entry::Down(l) => Location {
+                x: self.x,
+                y: self.y - l,
+            },
+            Day03Entry::Up(l) => Location {
+                x: self.x,
+                y: self.y + l,
+            },
+            Day03Entry::Left(l) => Location {
+                x: self.x - l,
+                y: self.y,
+            },
+            Day03Entry::Right(l) => Location {
+                x: self.x + l,
+                y: self.y,
+            },
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Line {
+    start: Location,
+    end: Location,
+}
+
+impl std::fmt::Display for Location {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({},{})", self.x, self.y)
+    }
+}
+
+impl std::fmt::Display for Line {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}->{}", self.start, self.end)
+    }
+}
+
+impl Line {
+    fn length(&self) -> isize {
+        (self.end.x - self.start.x).abs() + (self.end.y - self.start.y).abs()
+    }
+
+    /// https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection#Given_two_points_on_each_line
+    fn intersection(&self, other: &Line) -> Option<Location> {
+        match (&self.start, &other.start) {
+            (&Location { x: 0, y: 0 }, &Location { x: 0, y: 0 }) => {
+                // Ignore initial point
+                None
+            }
+            _ => {
+                let x1 = self.start.x;
+                let x2 = self.end.x;
+                let x3 = other.start.x;
+                let x4 = other.end.x;
+
+                let y1 = self.start.y;
+                let y2 = self.end.y;
+                let y3 = other.start.y;
+                let y4 = other.end.y;
+                log::debug!("Intersection between {} & {}", self, other);
+
+                let denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+
+                match denominator {
+                    0 => {
+                        log::debug!("Denominator is null, lines are parallel");
+                        None
+                    }
+                    denominator => {
+                        let t = (((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) as f64)
+                            / (denominator as f64);
+                        let u = -(((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) as f64)
+                            / (denominator as f64);
+                        log::debug!("  --> denominator: {}  t: {}  u: {}", denominator, t, u);
+                        if (0.0 <= t) && (t <= 1.0) && (0.0 <= u) && (u <= 1.0) {
+                            let new_x = x1 + (t * ((x2 - x1) as f64)).round() as isize;
+                            let new_y = y1 + (t * ((y2 - y1) as f64)).round() as isize;
+                            log::debug!("  --> intersection: ({},{})", new_x, new_y);
+
+                            Some(Location { x: new_x, y: new_y })
+                        } else {
+                            None
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Displacements<'a> {
+    name: &'static str,
+    start: Location,
+    displacements: &'a [Day03Entry],
+    idx: usize,
+}
+
+impl Displacements<'_> {
+    fn end(&self) -> Location {
+        (&self.start) + &self.displacements[self.idx]
+    }
+}
+
+impl<'a> Iterator for Displacements<'a> {
+    type Item = Line;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.idx == self.displacements.len() {
+            None
+        } else {
+            let start = self.start.clone();
+            let end = self.end();
+
+            self.idx += 1;
+            self.start = end.clone();
+
+            Some(Line { start, end })
+        }
+    }
 }
 
 impl<'a> AoC<'a> for Day03Initial<'a> {
@@ -14,14 +152,123 @@ impl<'a> AoC<'a> for Day03Initial<'a> {
     }
 
     fn new(input: &'a str) -> Day03Initial<'_> {
-        Day03Initial { input }
+        let displacements: Vec<Vec<Day03Entry>> = parse_input(input);
+        Day03Initial {
+            input,
+            displacements,
+        }
     }
 
-    // fn solution_part1(&self) -> Self::SolutionPart1 {
-    // }
+    fn solution_part1(&self) -> Self::SolutionPart1 {
+        let path1 = Displacements {
+            name: "path1",
+            start: Location::default(),
+            displacements: &self.displacements[0],
+            idx: 0,
+        };
 
-    // fn solution_part2(&self) -> Self::SolutionPart2 {
-    // }
+        let mut minimum_distance = isize::max_value();
+
+        for (l1, line1) in path1.enumerate() {
+            let path2 = Displacements {
+                name: "path2",
+                start: Location::default(),
+                displacements: &self.displacements[1],
+                idx: 0,
+            };
+            for (l2, line2) in path2.enumerate() {
+                log::debug!("------------------------------------------------------------------");
+                log::debug!(
+                    "(l1,l2): ({},{})  [line1, line2]: [{}, {}]",
+                    l1,
+                    l2,
+                    line1,
+                    line2
+                );
+                match line1.intersection(&line2) {
+                    None => {
+                        log::debug!("No intersection found");
+                    }
+                    Some(intersection_position) => {
+                        let intersection_distance =
+                            intersection_position.x + intersection_position.y;
+                        log::debug!(
+                            "minimum_distance: {}  intersection_distance: {}",
+                            minimum_distance,
+                            intersection_distance
+                        );
+                        minimum_distance = minimum_distance.min(intersection_distance);
+                    }
+                }
+            }
+        }
+
+        minimum_distance
+    }
+
+    fn solution_part2(&self) -> Self::SolutionPart2 {
+        let path1 = Displacements {
+            name: "path1",
+            start: Location::default(),
+            displacements: &self.displacements[0],
+            idx: 0,
+        };
+
+        let mut min_total_steps = isize::max_value();
+
+        let mut total_steps_path1 = 0;
+
+        for (l1, line1) in path1.enumerate() {
+            let path2 = Displacements {
+                name: "path2",
+                start: Location::default(),
+                displacements: &self.displacements[1],
+                idx: 0,
+            };
+            let mut total_steps_path2 = 0;
+            for (l2, line2) in path2.enumerate() {
+                log::debug!("------------------------------------------------------------------");
+                log::debug!(
+                    "(l1,l2): ({},{})  [line1, line2]: [{}, {}]",
+                    l1,
+                    l2,
+                    line1,
+                    line2
+                );
+                match line1.intersection(&line2) {
+                    None => {
+                        log::debug!("No intersection found");
+                    }
+                    Some(intersection_position) => {
+                        let intersection_distance =
+                            intersection_position.x + intersection_position.y;
+                        let total_steps = total_steps_path1
+                            + total_steps_path2
+                            + Line {
+                                start: line1.start.clone(),
+                                end: intersection_position.clone(),
+                            }
+                            .length()
+                            + Line {
+                                start: line2.start.clone(),
+                                end: intersection_position.clone(),
+                            }
+                            .length();
+                        log::debug!(
+                            "total_steps: {}  intersection_distance: {}",
+                            total_steps,
+                            intersection_distance
+                        );
+                        min_total_steps = min_total_steps.min(total_steps);
+                    }
+                }
+                total_steps_path2 += line2.length();
+            }
+            total_steps_path1 += line1.length();
+        }
+
+        min_total_steps
+    }
 }
 
 #[cfg(test)]
@@ -35,9 +282,7 @@ mod tests {
             fn solution() {
                 init_logger();
 
-                unimplemented!();
-
-                let expected = 0;
+                let expected = 1264;
                 let to_check = Day03Initial::new(PUZZLE_INPUT).solution_part1();
 
                 assert_eq!(to_check, expected);
@@ -52,22 +297,43 @@ mod tests {
             fn ex01() {
                 init_logger();
 
-                unimplemented!();
+                let expected = 6;
+                let input = "R8,U5,L5,D3
+                U7,R6,D4,L4";
+                let to_check = Day03Initial::new(input).solution_part1();
 
-                let expected = 0;
-                let input = "";
+                assert_eq!(to_check, expected);
+            }
+
+            #[test]
+            fn ex02() {
+                init_logger();
+
+                log::error!(
+                    "Test 'initial::tests::part1::given::ex02' does NOT give 159 but instead 146 (bug in Aoc?)"
+                );
+                // let expected = 159;
+                let expected = 146;
+
+                let input = "R75,D30,R83,U83,L12,D49,R71,U7,L72
+                U62,R66,U55,R34,D71,R55,D58,R83";
+                let to_check = Day03Initial::new(input).solution_part1();
+
+                assert_eq!(to_check, expected);
+            }
+
+            #[test]
+            fn ex03() {
+                init_logger();
+
+                let expected = 135;
+                let input = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+                U98,R91,D20,R16,D67,R40,U7,R15,U6,R7";
                 let to_check = Day03Initial::new(input).solution_part1();
 
                 assert_eq!(to_check, expected);
             }
         }
-
-        /*
-        mod extra {
-            use super::super::super::Day03Initial;
-            use crate::{tests::init_logger, AoC, PUZZLE_INPUT};
-        }
-        */
     }
 
     mod part2 {
@@ -79,9 +345,7 @@ mod tests {
             fn solution() {
                 init_logger();
 
-                unimplemented!();
-
-                let expected = 0;
+                let expected = 37390;
                 let to_check = Day03Initial::new(PUZZLE_INPUT).solution_part2();
 
                 assert_eq!(to_check, expected);
@@ -96,21 +360,37 @@ mod tests {
             fn ex01() {
                 init_logger();
 
-                unimplemented!();
+                let expected = 30;
+                let input = "R8,U5,L5,D3
+                U7,R6,D4,L4";
+                let to_check = Day03Initial::new(input).solution_part2();
 
-                let expected = 0;
-                let input = "";
+                assert_eq!(to_check, expected);
+            }
+
+            #[test]
+            fn ex02() {
+                init_logger();
+
+                let expected = 610;
+                let input = "R75,D30,R83,U83,L12,D49,R71,U7,L72
+                U62,R66,U55,R34,D71,R55,D58,R83";
+                let to_check = Day03Initial::new(input).solution_part2();
+
+                assert_eq!(to_check, expected);
+            }
+
+            #[test]
+            fn ex03() {
+                init_logger();
+
+                let expected = 410;
+                let input = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+                U98,R91,D20,R16,D67,R40,U7,R15,U6,R7";
                 let to_check = Day03Initial::new(input).solution_part2();
 
                 assert_eq!(to_check, expected);
             }
         }
-
-        /*
-        mod extra {
-            use super::super::super::Day03Initial;
-            use crate::{tests::init_logger, AoC, PUZZLE_INPUT};
-        }
-        */
     }
 }
--- day03/src/lib.rs
+++ day03/src/lib.rs
@@ -1,29 +1,152 @@
-//! # Day 03:
+//! # Day 03: Crossed Wires
+//!
+//! [GitHub source](https://github.com/nbigaouette/advent_of_code_2019/tree/master/day03)
 //!
 //! [Benchmarking report](../../../day03/target/criterion/report/index.html)
 //!
 //! ## Part One
 //!
+//! The gravity assist was successful, and you're well on your way to the Venus refuelling station.
+//! During the rush back on Earth, the fuel management system wasn't completely installed, so that's
+//! next on the priority list.
 //!
+//! Opening the front panel reveals a jumble of wires. Specifically, _two wires_ are connected to a
+//! central port and extend outward on a grid. You trace the path each wire takes as it leaves the
+//! central port, one wire per line of text (your puzzle input).
+//!
+//! The wires twist and turn, but the two wires occasionally cross paths. To fix the circuit, you need
+//! to _find the intersection point closest to the central port_. Because the wires are on a grid, use
+//! the [Manhattan distance](https://en.wikipedia.org/wiki/Taxicab_geometry) for this measurement.
+//! While the wires do technically cross right at the central port where they both start, this point
+//! does not count, nor does a wire count as crossing with itself.
+//!
+//! For example, if the first wire's path is `R8,U5,L5,D3`, then starting from the central port (`o`),
+//! it goes right `8`, up `5`, left `5`, and finally down `3`:
+//!
+//! ```text
+//! ...........
+//! ...........
+//! ...........
+//! ....+----+.
+//! ....|....|.
+//! ....|....|.
+//! ....|....|.
+//! .........|.
+//! .o-------+.
+//! ...........
+//! ```
+//!
+//! Then, if the second wire's path is `U7,R6,D4,L4`, it goes up `7`, right `6`, down `4`, and left
+//! `4`:
+//!
+//! ```text
+//! ...........
+//! .+-----+...
+//! .|.....|...
+//! .|..+--X-+.
+//! .|..|..|.|.
+//! .|.-X--+.|.
+//! .|..|....|.
+//! .|.......|.
+//! .o-------+.
+//! ...........
+//! ```
+//!
+//! These wires cross at two locations (marked `X`), but the lower-left one is closer to the central
+//! port: its distance is `3 + 3 = 6`.
+//!
+//! Here are a few more examples:
+//!
+//! * distance `159`:
+//!     ```text
+//!     R75,D30,R83,U83,L12,D49,R71,U7,L72  
+//!     U62,R66,U55,R34,D71,R55,D58,R83
+//!     ```
+//! * distance `135`:
+//!     ```text
+//!     R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51  
+//!     U98,R91,D20,R16,D67,R40,U7,R15,U6,R7
+//!     ```
+//!
+//! _What is the Manhattan distance_ from the central port to the closest intersection?
 //!
 //! ## Part Two
 //!
+//! It turns out that this circuit is very timing-sensitive; you actually need to _minimize the signal delay_.
 //!
+//! To do this, calculate the _number of steps_ each wire takes to reach each intersection; choose the intersection where the _sum of both wires' steps_ is lowest. If a wire visits a position on the grid multiple times, use the steps value from the _first_ time it visits that position when calculating the total value of a specific intersection.
+//!
+//! The number of steps a wire takes is the total number of grid squares the wire has entered to get to that location, including the intersection being considered. Again consider the example from above:
+//!
+//! ```text
+//! ...........
+//! .+-----+...
+//! .|.....|...
+//! .|..+--X-+.
+//! .|..|..|.|.
+//! .|.-X--+.|.
+//! .|..|....|.
+//! .|.......|.
+//! .o-------+.
+//! ...........
+//! ```
+//!
+//! In the above example, the intersection closest to the central port is reached after `8+5+5+2 = _20_` steps by the first wire and `7+6+4+3 = _20_` steps by the second wire for a total of `20+20 = _40_` steps.
+//!
+//! However, the top-right intersection is better: the first wire takes only `8+5+2 = _15_` and the second wire takes only `7+6+2 = _15_`, a total of `15+15 = _30_` steps.
+//!
+//! Here are the best steps for the extra examples from above:
+//!
+//! * `610` steps:
+//!     ```text
+//!     R75,D30,R83,U83,L12,D49,R71,U7,L72  
+//!     U62,R66,U55,R34,D71,R55,D58,R83
+//!     ```
+//! * `410` steps:
+//!     ```text
+//!     R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51  
+//!     U98,R91,D20,R16,D67,R40,U7,R15,U6,R7
+//!     ```
+//!
+//! _What is the fewest combined steps the wires must take to reach an intersection?_
 //!
 
-use std::fmt::Debug;
+use std::{fmt::Debug, str::FromStr};
 
 pub use anyhow::{Context, Result};
-use shrinkwraprs::Shrinkwrap;
 
 pub mod initial;
 pub use crate::initial::Day03Initial;
 
-#[derive(Debug, Shrinkwrap, PartialEq)]
-pub struct Day03Entry(usize);
+pub type Length = isize;
 
-type Day03SolutionPart1 = i64;
-type Day03SolutionPart2 = i64;
+#[derive(Debug, PartialEq)]
+pub enum Day03Entry {
+    Up(Length),
+    Down(Length),
+    Left(Length),
+    Right(Length),
+}
+
+impl FromStr for Day03Entry {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (direction_str, length_str) = s.trim().split_at(1);
+        let length: Length = length_str.parse()?;
+        let direction = match direction_str {
+            "R" => Day03Entry::Right(length),
+            "L" => Day03Entry::Left(length),
+            "U" => Day03Entry::Up(length),
+            "D" => Day03Entry::Down(length),
+            unknown => anyhow::bail!("Unknown direction: {:?}", unknown),
+        };
+        Ok(direction)
+    }
+}
+
+type Day03SolutionPart1 = isize;
+type Day03SolutionPart2 = isize;
 
 pub trait AoC<'a>: Debug {
     type SolutionPart1;
@@ -46,10 +169,18 @@ pub trait AoC<'a>: Debug {
     }
 }
 
-pub fn parse_input<'a>(input: &'a str) -> impl Iterator<Item = Day03Entry> + 'a {
+pub fn parse_line<'a>(line: &'a str) -> impl Iterator<Item = Day03Entry> + 'a {
+    line.trim()
+        .split(',')
+        .filter_map(|entry| entry.parse::<Day03Entry>().ok())
+}
+
+pub fn parse_input(input: &str) -> Vec<Vec<Day03Entry>> {
     input
+        .trim()
         .lines()
-        .map(|line| Day03Entry(line.trim().parse().expect("Invalid entry")))
+        .map(|line: &str| parse_line(line).collect())
+        .collect()
 }
 
 pub static PUZZLE_INPUT: &str = include_str!("../input");
@@ -65,30 +196,17 @@ pub mod benchmark {
     >;
 
     pub fn to_benchmark<'a>() -> BenchmarkVector<'a> {
-        unimplemented!()
-        // vec![
-        //     Box::new(Day03Initial::new(PUZZLE_INPUT)),
-        // ]
+        vec![Box::new(Day03Initial::new(PUZZLE_INPUT))]
     }
 }
 
 #[cfg(test)]
 mod tests {
     use env_logger;
-    use std::env;
 
-    use crate::parse_input;
+    use super::*;
 
     pub fn init_logger() {
-        env::var("RUST_LOG")
-            .or_else(|_| -> Result<String, ()> {
-                let rust_log = "debug".to_string();
-                println!("Environment variable 'RUST_LOG' not set.");
-                println!("Setting to: {}", rust_log);
-                env::set_var("RUST_LOG", &rust_log);
-                Ok(rust_log)
-            })
-            .unwrap();
         let _ = env_logger::try_init();
     }
 
@@ -96,16 +214,34 @@ mod tests {
     fn parse() {
         init_logger();
 
-        unimplemented!();
+        let input = "R75,D30,R83,U83,L12,D49,R71,U7,L72
+        U62,R66,U55,R34,D71,R55,D58,R83";
+        let parsed = parse_input(input);
 
-        let parsed: Vec<Day03Entry> = parse_input(PUZZLE_INPUT).collect();
-        assert_eq!(parsed.len(), 0);
-        assert_eq!(
-            &parsed[0..5],
-            &[
-                //
-                Day01Entry(0),
-            ]
-        );
+        let expected = vec![
+            vec![
+                Day03Entry::Right(75),
+                Day03Entry::Down(30),
+                Day03Entry::Right(83),
+                Day03Entry::Up(83),
+                Day03Entry::Left(12),
+                Day03Entry::Down(49),
+                Day03Entry::Right(71),
+                Day03Entry::Up(7),
+                Day03Entry::Left(72),
+            ],
+            vec![
+                Day03Entry::Up(62),
+                Day03Entry::Right(66),
+                Day03Entry::Up(55),
+                Day03Entry::Right(34),
+                Day03Entry::Down(71),
+                Day03Entry::Right(55),
+                Day03Entry::Down(58),
+                Day03Entry::Right(83),
+            ],
+        ];
+
+        assert_eq!(parsed, expected);
     }
 }
